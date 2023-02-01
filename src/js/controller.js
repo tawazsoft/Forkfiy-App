4a731b37050bdfe40993ebbbfec17c1626d4328e
Tawanda <tawanda.m@uncommon.org>
2023-02-01 13:56:33 +0200

    second commit

--- src/js/controller.js
+++ src/js/controller.js
@@ -12,3 +12,17 @@ const timeout = function (s) {
 
 ///////////////////////////////////////
 
+const showRescipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    const data = await res.json();
+
+    console.log(res, data);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showRescipe();
