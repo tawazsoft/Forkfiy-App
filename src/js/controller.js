63c9cf3d4e44c5d170d63bc08e47f4c75da17156
Tawanda <tawanda.m@uncommon.org>
2023-02-12 20:59:45 +0200

    loading recipe

--- src/js/controller.js
+++ src/js/controller.js
@@ -19,6 +19,8 @@ const showRescipe = async function () {
     );
     const data = await res.json();
 
+    if (!res.ok) throw new Error(`${data.message}${res.status})`)
+
     console.log(res, data);
   } catch (err) {
     alert(err);
